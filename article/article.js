909d16a387d78abd6d61f7965d46bb7b423399dd
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-14 18:06:40 +0200

    WIP

--- article/article.js
+++ article/article.js
@@ -1,30 +1,15 @@
 const articles = document.querySelectorAll('#petsContainer .pet-article');
 
-const showText = 'Show';
-const hideText = 'Hide';
-
+//  EXERCISE 1
 class ArticleEnhancer {
-  constructor(article) {
-    this.article = article;
-    this.paragraphs = this.article.querySelectorAll('p');
-    this.addMagicButton();
-    this.toggleParagraphs();
-  }
 
-  toggleParagraphs() {
-    this.paragraphs.forEach(p => {
-      p.classList.toggle('hidden');
-    });
-  }
-
-  addMagicButton() {
-    const button = document.createElement('button');
-    button.textContent = 'Toggle';
-    button.addEventListener('click', this.toggleParagraphs.bind(this))
-    this.article.insertAdjacentElement('beforeend', button);
-  }
 }
 
+articles.forEach((article) => {
+  new ArticleEnhancer(article);
+})
+
+// EXERCISE 2
 const articleData = [
   {
     heading: 'Sheep',
@@ -44,30 +29,9 @@ const articleData = [
 ];
 
 class ArticleGenerator {
-  constructor(domNode, data) {
-    this.article = domNode;
-    this.articleData = data;
-    this.createHeading();
-    this.createParagraphs();
-  }
 
-  createHeading() {
-    const heading = document.createElement('h3');
-    heading.textContent = this.articleData.heading;
-    this.article.appendChild(heading);
-  }
-
-  createParagraphs() {
-    const p1 = document.createElement('p');
-    const p2 = document.createElement('p');
-    p1.textContent = this.articleData.paragraph1;
-    p2.textContent = this.articleData.paragraph2;
-    this.article.insertAdjacentElement('beforeend', p1);
-    this.article.insertAdjacentElement('beforeend', p2);
-  }
 }
 
 articles.forEach((article, idx) => {
-  new ArticleGenerator(article, articleData[idx]);
-  new ArticleEnhancer(article);
+
 })
