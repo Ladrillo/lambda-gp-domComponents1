3205a77a0b60436537811e3bd990092fcb4b4af6
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-14 18:00:42 +0200

    wip

--- article/article.js
+++ article/article.js
@@ -25,6 +25,54 @@ class ArticleEnhancer {
   }
 }
 
-articles.forEach(article => {
+const articleData = [
+  {
+    heading: 'Sheep',
+    paragraph1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    paragraph2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    heading: 'Lambs',
+    paragraph1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    paragraph2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    heading: 'Wolves',
+    paragraph1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    paragraph2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
+
+
+// articles.forEach(article => {
+//   new ArticleEnhancer(article);
+// });
+
+class ArticleGenerator {
+  constructor(domNode, data) {
+    this.article = domNode;
+    this.articleData = data;
+    this.createHeading();
+    this.createParagraphs();
+  }
+
+  createHeading() {
+    const heading = document.createElement('h3');
+    heading.textContent = this.articleData.heading;
+    this.article.appendChild(heading);
+  }
+
+  createParagraphs() {
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    p1.textContent = this.articleData.paragraph1;
+    p2.textContent = this.articleData.paragraph2;
+    this.article.insertAdjacentElement('beforeend', p1);
+    this.article.insertAdjacentElement('beforeend', p2);
+  }
+}
+
+articles.forEach((article, idx) => {
+  new ArticleGenerator(article, articleData[idx]);
   new ArticleEnhancer(article);
-});
+})
