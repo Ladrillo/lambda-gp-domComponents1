ef723aaa7d86747f8e97923f35acdf0bf1b096d8
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-03 15:50:42 +0200

    prep for lecture

--- article/article.js
+++ article/article.js
@@ -1,57 +1,31 @@
-const articleData = [
+const articlesFromApi = [
   {
-    heading: 'Sheep',
-    paragraph1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    paragraph2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Sheep',
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
-    heading: 'Lambs',
-    paragraph1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    paragraph2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Lambs',
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
-    heading: 'Wolves',
-    paragraph1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    paragraph2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Wolves',
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
 ];
 
-function articleMaker(data) {
-  const { heading, paragraph1, paragraph2 } = data;
+// 1
+// Build an articleMaker function that takes an object with title, text1 and text2
+// This function should return an article element with an h3 heading, and two paragraphs
+// The heading should display the title, and the two paragraphs should display text1 and text2 resp.
 
-  const article = document.createElement('article');
-  const h3 = document.createElement('h3');
-  const p1 = document.createElement('p');
-  const p2 = document.createElement('p');
-  const button = document.createElement('button');
+// 2
+// Using the articleMaker, generate articles using the articlesFromApi array of article data
 
-  h3.textContent = heading;
-  p1.textContent = paragraph1;
-  p2.textContent = paragraph2;
-  p1.classList.add('hidden');
-  p2.classList.add('hidden');
+// 3
+// append the articles to the DOM
 
-  button.textContent = 'toggle'
-  button.addEventListener('click', e => {
-    p1.classList.toggle('hidden');
-    p2.classList.toggle('hidden');
-  });
-
-  article.insertAdjacentElement('afterbegin', h3);
-  article.insertAdjacentElement('beforeend', p1);
-  article.insertAdjacentElement('beforeend', p2);
-  article.insertAdjacentElement('beforeend', button);
-
-  return article;
-}
-
-const articles = articleData.map(data => articleMaker(data))
- 
-articles.forEach(article => {
-  document.querySelector('.articles')
-    .insertAdjacentElement('beforeend', article);
-});
-
-document.querySelectorAll('article:nth-child(odd)')
-  .forEach(thing => thing.classList.add('special'))
-
+// 4
+// Improve the articleMaker so each article has a 'toggle' button to display or not the paragraphs.
